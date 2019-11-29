bd0d64c3333d38d949ef8356cd5c23edbc6c0f7e
Vorn Mom <vorn@vorn.us>
2019-11-29 13:18:27 -0500

    Added Garmin Lidar Lite v3 Driver (#3)
    Added Garmin Lidar Lite v3 Driver

---
+++ src/NaoVictoria/NavEngine/Sensors/LidarLiteV3/PowerOption.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NaoVictoria.NavEngine.Sensors.LidarLiteV3
+{
+    public enum PowerOption
+    {
+        Default = 0x80,
+        DisableReceiverCircuit = 0x00,
+        Sleep = 0x04
+    }
+}
